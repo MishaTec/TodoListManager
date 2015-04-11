d019be28ba0f86d066bd969de3a1f46f411eab8e
michaz13 <mzmichael700@gmail.com>
2015-04-11 13:23:14 +0300

    v2 final

--- app/src/main/java/il/ac/huji/todolist/AddNewTodoItemActivity.java
+++ app/src/main/java/il/ac/huji/todolist/AddNewTodoItemActivity.java
@@ -1,17 +1,48 @@
 package il.ac.huji.todolist;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 
-public class AddNewTodoItemActivity extends ActionBarActivity {
+public class AddNewTodoItemActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_todo_item);
+
+        final Button btnOK = (Button)findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent result = new Intent();
+                String title = ((EditText) findViewById(R.id.edtNewItem)).getText().toString();
+                result.putExtra("title", title);
+                DatePicker dp = (DatePicker) findViewById(R.id.datePicker);
+                Calendar cal = Calendar.getInstance();
+                cal.set(dp.getYear(),dp.getMonth(),dp.getDayOfMonth(),0,0,0);
+                result.putExtra("dueDate", cal.getTime()); // send java.util.Date
+                setResult(RESULT_OK, result);
+                finish();
+            }
+        });
+
+        final Button btnCancel = (Button)findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent result = new Intent();
+                setResult(RESULT_CANCELED, result);
+                finish();
+            }
+        });
     }
 
 
@@ -27,12 +58,12 @@ public class AddNewTodoItemActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+/*        if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
+++ app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,79 +1,148 @@
 package il.ac.huji.todolist;
+
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
-import android.view.KeyEvent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class TodoListManagerActivity extends ActionBarActivity {
-    private class TodoListAdapter extends ArrayAdapter<String> {
-        public TodoListAdapter(TodoListManagerActivity todoListManagerActivity, int simple_list_item_1, List<String> theList) {
-            super(todoListManagerActivity,simple_list_item_1,theList);
+    private static final int NEW_ITEM_REQUEST = 42;
+    private ArrayAdapter<TodoListItem> adapter;
+    private ArrayList<TodoListItem> list = new ArrayList<>();
+
+    private class TodoListItem {
+        private String title;
+        private Date dueDate;
+
+        private TodoListItem(String title, Date dueDate) {
+            this.title = title;
+            this.dueDate = dueDate;
+        }
+    }
+
+    private class TodoListAdapter extends ArrayAdapter<TodoListItem> {
+        public TodoListAdapter(TodoListManagerActivity todoListManagerActivity, int simple_list_item_1, List<TodoListItem> theList) {
+            super(todoListManagerActivity, simple_list_item_1, theList);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            TextView view = (TextView)super.getView(position, convertView, parent);
-            if (position % 2 == 0) {
-                view.setTextColor(Color.RED);
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            View view = inflater.inflate(R.layout.todo_list_item, null);
+
+            TextView titleText = (TextView) view.findViewById(R.id.txtTodoTitle);
+            titleText.setText(getItem(position).title);
+
+            TextView dueDateText = (TextView) view.findViewById(R.id.txtTodoDueDate);
+            Date dueDate = getItem(position).dueDate;
+            if (dueDate != null) {
+                SimpleDateFormat dateOnly = new SimpleDateFormat("dd/MM/yyyy");
+                dueDateText.setText(dateOnly.format(dueDate));
+                if (isOverdue(dueDate)) {
+                    dueDateText.setTextColor(Color.RED);
+                    titleText.setTextColor(Color.RED);
+                }
             } else {
-                view.setTextColor(Color.BLUE);
+                dueDateText.setText("No due date");
             }
             return view;
         }
+
+        /**
+         * Checks if the dueDate is before the current day (today)
+         *
+         * @param dueDate - assumes dueDate's time is set to 00:00:00
+         * @return true iff dueDate is before the current day
+         */
+        private boolean isOverdue(Date dueDate) {
+            Calendar todayCal = Calendar.getInstance();
+            todayCal.set(Calendar.HOUR_OF_DAY, 0);
+            todayCal.set(Calendar.MINUTE, 0);
+            todayCal.set(Calendar.SECOND, 0);
+            todayCal.set(Calendar.MILLISECOND, 0);
+            Calendar dueCal = Calendar.getInstance();
+            dueCal.setTime(dueDate);
+            return dueCal.before(todayCal);
+        }
     }
 
-    private boolean addNewItem(){
-        EditText item = (EditText) findViewById(R.id.edtNewItem);
-        String itemText = item.getText().toString();
-        if(!itemText.equals("")) {
-            System.out.println(itemText);
-            list.add(itemText);
-        }
-        item.setText("");
-        adapter.notifyDataSetChanged();
+    /**
+     * Opens a dialog for adding a to-do
+     *
+     * @return true
+     */
+    private boolean addNewItem() {
+        Intent intent = new Intent(this, AddNewTodoItemActivity.class);
+        startActivityForResult(intent, NEW_ITEM_REQUEST);
         return true;
     }
 
+    @Override
+    protected void onActivityResult(int reqCode, int resCode, Intent data) {
+        if (resCode == Activity.RESULT_OK && reqCode == NEW_ITEM_REQUEST) {
+            String title = data.getStringExtra("title");
+            Date dueDate = (Date) data.getSerializableExtra("dueDate");
+            if (!title.equals("")) {
+                list.add(new TodoListItem(title, dueDate));
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
 
-    ArrayAdapter<String> adapter;
-    ArrayList<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list_manager);
 
-        ListView todoList = (ListView)findViewById(R.id.lstTodoItems);
-        adapter = new TodoListAdapter(this, android.R.layout.simple_list_item_1, this.list);
+        ListView todoList = (ListView) findViewById(R.id.lstTodoItems);
+        adapter = new TodoListAdapter(this, R.layout.todo_list_item, this.list);
         todoList.setAdapter(adapter);
 
         todoList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> av, View v, int pos, final long id) {
                 final AlertDialog.Builder builder = new AlertDialog.Builder(TodoListManagerActivity.this);
-                final int thatPos = pos;
-                DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+                final int posFinal = pos;
+                String title = ((TextView) v.findViewById(R.id.txtTodoTitle)).getText().toString();
+                builder.setMessage(title);
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
-                        TodoListManagerActivity.this.list.remove(thatPos);
+                        TodoListManagerActivity.this.list.remove(posFinal);
                         adapter.notifyDataSetChanged();
                     }
-                };
-                builder.setMessage(TodoListManagerActivity.this.list.get(pos));
-                builder.setPositiveButton("delete", listener);
+                });
+
+                if (title.toLowerCase().matches("call\\s[^\\s]+.*")) { // starts with 'call'
+                    final String tel = title.substring(5);
+                    builder.setNegativeButton("Call " + tel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + tel));
+                            startActivity(dial);
+                        }
+                    });
+                }
+
                 builder.show();
                 return true;
             }
@@ -95,7 +164,7 @@ public class TodoListManagerActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menuItemAdd) {
             addNewItem();
             return true;
         }
@@ -103,6 +172,7 @@ public class TodoListManagerActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+/*
     @Override
     public boolean onKeyDown(int keycode, KeyEvent e) {
         switch(keycode) {
@@ -113,5 +183,6 @@ public class TodoListManagerActivity extends ActionBarActivity {
 
         return super.onKeyDown(keycode, e);
     }
+*/
 
 }
